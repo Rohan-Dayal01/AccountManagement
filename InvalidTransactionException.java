b0b52a51c0d3e81369720f0129491d578302c960
Rohan-Dayal01 <50881330+Rohan-Dayal01@users.noreply.github.com>
2020-03-05 22:26:19 -0500

    Update InvalidTransactionException.java

--- InvalidTransactionException.java
+++ InvalidTransactionException.java
@@ -1,11 +1,5 @@
 /**
  * @author Rohan Dayal
- * ID Num: 112768867
- * Recitation 02
- * 
-*/
-package cse214;
-/**
  * class defines the InvalidTransactionException and extends Exception. 
  * called when the user seeks to create a Transaction with an amount =0 or a
  * date outside of the given constraints.
